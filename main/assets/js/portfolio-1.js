d1c03151868200819eb3418ffb4cf2c95ec90111
Kailash Parshad <99875896+at0m-b0mb@users.noreply.github.com>
2026-04-06 12:04:03 -0400

    Delete main directory

--- main/assets/js/portfolio-1.js
+++
@@ -1,5 +0,0 @@
-(function () {
-    $("button.cta").on('click', function () {
-        window.location.href = "projects";
-    });
-})();
--- main/assets/js/portfolio.js
+++
@@ -1,5 +0,0 @@
-(function () {
-    $("button.cta").on('click', function () {
-        window.location.href = "https://linktr.ee/HackProKP";
-    });
-})();
